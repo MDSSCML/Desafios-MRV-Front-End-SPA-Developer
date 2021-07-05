6f580da0acdcbf87664aa31727499b22c524d100
Márcio Trovão <mc872684@gmail.com>
2021-07-05 17:17:22 -0300

    deletando arquivo duplicado

--- Quanta Mandioca.js
+++ Quanta Mandioca.js
@@ -48,6 +48,5 @@ let bernardo = 600 * parseInt(gets());
 let marina = 1000 * parseInt(gets());
 let iara = 150 * parseInt(gets());
 let marlene = 225;
-<<<<<<< HEAD:Quanta Mandioca.html
 let total = (chico + bento + bernardo + marina + iara + marlene); // Digite aqui o calculo total
 console.log(total);
