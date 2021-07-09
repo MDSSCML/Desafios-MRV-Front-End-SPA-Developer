a3ccf331e91c592886497951efe738e9d24e6ce9
Márcio Trovão <mc872684@gmail.com>
2021-07-09 09:41:17 -0300

    Código atualizado

--- Exibindo Números.js
+++ Exibindo Números.js
@@ -16,8 +16,6 @@ Exemplo de Entrada	Exemplo de Saída
 
 
 let num = parseInt(gets());
-for (cont = 1; cont <= num; cont++) {
-    if (cont % 2 == 0) {
-        console.log(cont);
-    }
+for(cont = 1; cont <= num; cont++){
+  cont %2 == 0 ? console.log(cont) :"Márcio Trovão";
 }
