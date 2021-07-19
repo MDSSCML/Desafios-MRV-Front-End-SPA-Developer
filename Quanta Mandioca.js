3ff829ac8bdadffdf233871a2a0f6f4216ca0396
Márcio Trovão <mc872684@gmail.com>
2021-07-19 00:17:07 -0300

    Modificado

--- Quanta Mandioca.js
+++ Quanta Mandioca.js
@@ -48,5 +48,5 @@ let bernardo = 600 * parseInt(gets());
 let marina = 1000 * parseInt(gets());
 let iara = 150 * parseInt(gets());
 let marlene = 225;
-let total = (chico + bento + bernardo + marina + iara + marlene); // Digite aqui o calculo total
-console.log(total);
+console.log(`${chico + bento + bernardo + marina + iara + marlene}`)
+
